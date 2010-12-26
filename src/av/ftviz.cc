02133687b9541c914963a415ba3c793bf3b0372c
Mark Washenberger <mwashenb@gmail.com>
2010-12-26 18:20:01 -0500

    not much

--- src/av/ftviz.cc
+++ src/av/ftviz.cc
@@ -1,6 +1,16 @@
 // Copyright 2010 <Mark Washenberger>
 #include <stdlib.h>
 
+class SoundInputStream;
+
 int main(int argc, char **argv) {
+
+  // create input stream from mic input
+  SoundInputStream *input;
+  // create output stream that is actually video
+  // create null timer
+  // create TimedAudioPipe pipe
+  // pipe.run_until_done
+
   return EXIT_SUCCESS;
 }
