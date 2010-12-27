6e468efda28e98708a31a4e751ce61b3969a1a90
Mark Washenberger <mwashenb@gmail.com>
2010-12-26 21:03:02 -0500

    declare sound output stream interface

--- src/audio/SoundOutputStreamInterface.h
+++ src/audio/SoundOutputStreamInterface.h
@@ -1,4 +1,14 @@
 // Copyright 2010 <Mark Washenberger>
 #ifndef SRC_AUDIO_SOUNDOUTPUTINTERFACE_H_
 #define SRC_AUDIO_SOUNDOUTPUTINTERFACE_H_
+
+namespace pg {
+namespace audio {
+
+class SoundOutputStreamInterface {
+};
+
+}  // namespace audio
+}  // namespace pg
+
 #endif  // SRC_AUDIO_SOUNDOUTPUTINTERFACE_H_
--- src/av/ftviz.cc
+++ src/av/ftviz.cc
@@ -2,15 +2,19 @@
 #include <stdlib.h>
 
 #include "audio/MicInputStream.h"
+#include "audio/SoundInputStreamInterface.h"
+#include "audio/SoundOutputStreamInterface.h"
 
-using pg::audio::SoundInputStreamInterface;
 using pg::audio::MicInputStream;
+using pg::audio::SoundInputStreamInterface;
+using pg::audio::SoundOutputStreamInterface;
 
 int main(int argc, char **argv) {
   // create input stream from mic input
   SoundInputStreamInterface *input;
   input = new MicInputStream();
   // create output stream that is actually video
+  SoundOutputStreamInterface *output;
   // create null timer
   // create TimedAudioPipe pipe
   // pipe.run_until_done
