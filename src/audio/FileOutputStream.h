597dd53c3ac64f77ea3fa13c87472acb40b8493b
Mark Washenberger <mwashenb@gmail.com>
2010-12-30 00:37:02 -0500

    create a skeletal file output stream

---
+++ src/audio/FileOutputStream.h
@@ -0,0 +1,12 @@
+// Copyright 2010 <Mark Washenberger>
+
+#include "audio/SoundOutputStreamInterface.h"
+
+namespace pg {
+namespace audio {
+
+class FileOutputStream: public pg::audio::SoundOutputStreamInterface {
+};
+
+}  // namespace audio
+}  // namespace pg
--- src/audio/record.cc
+++ src/audio/record.cc
@@ -1,10 +1,12 @@
 // Copyright 2010 <Mark Washenberger>
 #include <stdlib.h>
 
+#include "audio/FileOutputStream.h"
 #include "audio/MicInputStream.h"
 #include "audio/SoundInputStreamInterface.h"
 #include "audio/SoundOutputStreamInterface.h"
 
+using pg::audio::FileOutputStream;
 using pg::audio::MicInputStream;
 using pg::audio::SoundInputStreamInterface;
 using pg::audio::SoundOutputStreamInterface;
@@ -15,11 +17,13 @@ int main(int argc, char **argv) {
   input = new MicInputStream();
   // create output stream that is sound file
   SoundOutputStreamInterface *output;
+  output = new FileOutputStream();
   // create null timer
   // create TimedAudioPipe pipe
   // pipe.run_until_done
 
   delete input;
+  delete output;
 
   return EXIT_SUCCESS;
 }
