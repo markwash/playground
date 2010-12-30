80e291e0bd0359c5af938dbdd1c5bb4ce97eb869
Mark Washenberger <mwashenb@gmail.com>
2010-12-30 00:25:49 -0500

    switch focus to simple recording

---
+++ src/audio/record.cc
@@ -0,0 +1,25 @@
+// Copyright 2010 <Mark Washenberger>
+#include <stdlib.h>
+
+#include "audio/MicInputStream.h"
+#include "audio/SoundInputStreamInterface.h"
+#include "audio/SoundOutputStreamInterface.h"
+
+using pg::audio::MicInputStream;
+using pg::audio::SoundInputStreamInterface;
+using pg::audio::SoundOutputStreamInterface;
+
+int main(int argc, char **argv) {
+  // create input stream from mic input
+  SoundInputStreamInterface *input;
+  input = new MicInputStream();
+  // create output stream that is sound file
+  SoundOutputStreamInterface *output;
+  // create null timer
+  // create TimedAudioPipe pipe
+  // pipe.run_until_done
+
+  delete input;
+
+  return EXIT_SUCCESS;
+}
