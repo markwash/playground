3129ada9a2ec33b3c0ee435d48456e4289ecfd3f
Mark Washenberger <mwashenb@gmail.com>
2011-03-29 11:32:12 -0400

    start unit testing FileInputStream

---
+++ src/test/audio/file-input-stream-test.cc
@@ -0,0 +1,12 @@
+// Copyright 2011 <Mark Washenberger>
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MODULE FileInputStreamTest
+#include <boost/test/unit_test.hpp>
+
+
+struct F {
+  F() {}
+};
+
+BOOST_FIXTURE_TEST_CASE(test, F) {
+}
