709dd9bbfb9ed9d05ea81336a47b3e5e2729dc10
Mark Washenberger <mwashenb@gmail.com>
2011-03-29 11:47:14 -0400

    create file input streams

---
+++ src/audio/FileInputStream.h
@@ -0,0 +1,17 @@
+// Copyright 2011 <Mark Washenberger>
+
+#ifndef SRC_AUDIO_FILEINPUTSTREAM_H_
+#define SRC_AUDIO_FILEINPUTSTREAM_H_
+
+#include "audio/SoundInputStreamInterface.h"
+
+namespace pg {
+namespace audio {
+
+class FileInputStream: public pg::audio::SoundInputStreamInterface {
+};
+
+}  // namespace audio
+}  // namespace pg
+
+#endif  // SRC_AUDIO_FILEINPUTSTREAM_H_
--- src/test/audio/file-input-stream-test.cc
+++ src/test/audio/file-input-stream-test.cc
@@ -3,10 +3,16 @@
 #define BOOST_TEST_MODULE FileInputStreamTest
 #include <boost/test/unit_test.hpp>
 
+#include "audio/FileInputStream.h"
+
+using pg::audio::FileInputStream;
+using pg::audio::SoundInputStreamInterface;
 
 struct F {
   F() {}
 };
 
 BOOST_FIXTURE_TEST_CASE(test, F) {
+  SoundInputStreamInterface *file = new FileInputStream();
+  delete file;
 }
