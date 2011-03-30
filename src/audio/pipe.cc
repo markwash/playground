9bc625270a636c0010446eea235cb2cd6ce5ad14
Mark Washenberger <mwashenb@gmail.com>
2011-03-30 15:30:39 -0400

    some basic extensibility added to pipe binary

--- src/audio/pipe.cc
+++ src/audio/pipe.cc
@@ -1,5 +1,8 @@
 // Copyright 2011 <Mark Washenberger>
 #include <stdlib.h>
+#include <string.h>
+
+#include <vector>
 
 #include "audio/FileInputStream.h"
 #include "audio/FileOutputStream.h"
@@ -8,21 +11,90 @@
 using pg::audio::FileInputStream;
 using pg::audio::FileOutputStream;
 using pg::audio::Pipe;
+using pg::audio::SoundInputStreamInterface;
+using pg::audio::SoundOutputStreamInterface;
+
+
+class Token {
+ public:
+  explicit Token(const char *token)
+      : token_(token), token_copy_(NULL), key_(NULL) {}
+  ~Token() { delete token_copy_; }
+
+  void Init() {
+    token_copy_ = strdup(token_);
+    char *strtok_context;
+    key_ = strtok_r(token_copy_, ":", &strtok_context);
+    char *arg;
+    while (true) {
+      arg = strtok_r(NULL, ",", &strtok_context);
+      if (arg == NULL)
+        break;
+      args_.push_back(arg);
+    }
+  }
+
+  const char *key() const { return key_; }
+  const std::vector<char *> *args() const { return &args_; }
+
+ private:
+  const char *token_;
+  char *token_copy_;
+  char *key_;
+  std::vector<char *> args_;
+};
+
+static SoundInputStreamInterface *CreateInputStreamOrDie(
+    const Token *token) {
+
+  if (strcmp(token->key(), "file") == 0) {
+    FileInputStream *input = new FileInputStream(token->args()->at(0));
+    input->Init();
+    return input;
+  }
+
+  fprintf(stderr, "Unrecognized key: %s\n", token->key());
+  exit(EXIT_FAILURE);
+  return NULL;
+}
+
+static SoundOutputStreamInterface *CreateOutputStreamOrDie(
+    const Token *token) {
+
+  if (strcmp(token->key(), "file") == 0) {
+    FileOutputStream *output = new FileOutputStream(token->args()->at(0));
+    output->Init();
+    return output;
+  }
+
+  fprintf(stderr, "Unrecognized key: %s\n", token->key());
+  exit(EXIT_FAILURE);
+  return NULL;
+}
 
 int main(int argc, char **argv) {
   if (argc != 3) {
-    fprintf(stderr, "Usage: %s <input_file> <output_file>\n", argv[0]);
+    fprintf(stderr, "Usage: %s <input> <output>\n", argv[0]);
     return EXIT_FAILURE;
   }
 
-  FileInputStream in(argv[1]);
-  in.Init();
+  Token input_token(argv[1]);
+  Token output_token(argv[2]);
 
-  FileOutputStream out(argv[2]);
-  out.Init();
+  input_token.Init();
+  output_token.Init();
 
-  Pipe pipe(&in, &out);
+  SoundInputStreamInterface *in;
+  SoundOutputStreamInterface *out;
+
+  in = CreateInputStreamOrDie(&input_token);
+  out = CreateOutputStreamOrDie(&output_token);
+
+  Pipe pipe(in, out);
   pipe.run_until_finished();
+
+  delete in;
+  delete out;
 
   return EXIT_SUCCESS;
 }
