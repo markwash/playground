e0c0b9243ff29f3bb380e72ad722dd5617006228
Mark Washenberger <mwashenb@gmail.com>
2011-03-31 20:44:41 -0400

    start testing bar grapher

---
+++ src/test/video/bar-grapher-test.cc
@@ -0,0 +1,11 @@
+// Copyright 2011 <Mark Washenberger>
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MODULE BarGrapherTest
+#include <boost/test/unit_test.hpp>
+
+struct F {
+  F() {}
+};
+
+BOOST_FIXTURE_TEST_CASE(test_, F) {
+}
